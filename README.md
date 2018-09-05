2d578bed2f54aa983461bc83fb1d7453c051685d
New-generation-hsc <hsc199761@163.com>
2018-09-05 18:08:11 +0800

    add heap structure

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# algorithm
+
+Implementing some common data structure and algorithm
+
+- ![heap](./heap/README.md)
+
+include the common data structure:
+
+1) ![max-heap](https://en.wikipedia.org/wiki/Min-max_heap)
+2) ![min-heap](https://en.wikipedia.org/wiki/Min-max_heap)
+3) ![priority queue](https://en.wikipedia.org/wiki/Priority_queue)
+
+and the sort algorithm:
+
+1) ![heap-sort](https://en.wikipedia.org/wiki/Heapsort)
---
+++ heap/heap.c
@@ -0,0 +1,16 @@
+/**
+
++-------------------+
+|    Binary Heap    |
++-------------------+
+
+this file implement the common tree-base data structure `binary heap`.
+
+binary heap must satisify two constraints:
+
+- Shape property: a binary heap is a complete binary tree
+- Heap property: the parent value must be larger or equal to its children if it is a max-heap
+                  or less then or equal to its children if it is a min-heap
+
+*/
+
